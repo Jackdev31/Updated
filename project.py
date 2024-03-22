a8f7e8fec8a98482a57291d9067769cfee342319
Jackdev31 <johnreysarona2@gmail.com>
2024-03-23 04:48:17 +0800

    IS FINALS

--- project.py
+++ project.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+import random
 
 app = Flask(__name__)
 
 data = pd.read_csv('base1.csv').dropna()
 
-X = data.drop(columns=["game_name", "game_image"])
-y = data["game_name"]
+X = data.drop(columns=["game_name", "game_image", "set_number"])
+y = data["set_number"]
 
 model = DecisionTreeClassifier()
 model.fit(X, y)
@@ -18,16 +19,21 @@ def index():
 
 @app.route('/suggested', methods=['POST'])
 def suggested():
-
     age = int(request.form['age'])
     gender = int(request.form['gender'])
 
     input_data = [[age, gender]]
     WhatGame = model.predict(input_data)[0]
 
-    game_img = f"images/{data[data['game_name'] == WhatGame]['game_image'].values[0]}"
+    SetGames = data[data['set_number'] == WhatGame][['game_name', 'game_image']].values.tolist()
+    SetGames = SetGames[:3] 
 
-    return render_template('index.html', WhatGame=WhatGame, game_img=game_img)
+    RelatedGames = data[data['set_number'] != WhatGame][['game_name', 'game_image']].sample(n=2).values.tolist()
+    
+    img = [[game[0], f"images/{game[1]}"] for game in SetGames]
+    img2 = [[game[0], f"images/{game[1]}"] for game in RelatedGames]
+
+    return render_template('result.html', WhatGame=WhatGame, SetGames=img, RelatedGames=img2)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=3000)
